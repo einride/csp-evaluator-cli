26a711c5e7d8963de1d538f5a0c089260233cd11
Nikita Galaiko <nikita.galaiko@einride.tech>
2024-10-03 11:17:25 +0200

    chore: ignore dependabot commit format

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  ignores: [
+    // ignore dependabot commits
+    (message) => message.includes("Signed-off-by: dependabot[bot] <support@github.com>"),
+  ],
 }
